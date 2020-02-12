c805fb4a4eb5c447df006bf41c3a7fe979e93b7e
Chris Johnson <chrislmjohnson@gmail.com>
2020-02-12 15:13:20 -0600

    added additional explanitory comments

--- pipeline.go
+++ pipeline.go
@@ -1,5 +1,6 @@
 package main
 
+//Simulates an external service this could be a different Go package, or an API
 func GetPipeline(email Email) Pipeline {
 	if email.From == "me@wildbit.com" {
 		return PipeA
--- qualifier.go
+++ qualifier.go
@@ -1,5 +1,6 @@
 package main
 
+//Simulates an external service this could be a different Go package, or an API
 func IsQualified(email Email) bool {
 	//TODO: implement Business Logic to check if email is qualified to be sent
 	return true
